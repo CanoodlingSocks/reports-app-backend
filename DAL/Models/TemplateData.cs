6b9adec3bdbc9906ae7c02a25075ce12ba4e9159
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-04-27 12:29:23 +0200

    add templates db

---
+++ DAL/Models/TemplateData.cs
@@ -0,0 +1,10 @@
+﻿namespace reports_app_backend.DAL.Models
+{
+    public class TemplateData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Fields { get; set; }
+    }
+}
---
+++ DAL/ReportsDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using reports_app_backend.DAL.Models;
+
+namespace reports_app_backend.DAL
+{
+    public class ReportsDBContext : DbContext
+    {
+        public DbSet<TemplateData> TemplateDatas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"DataSource=reportsdatabase.db;");   
+        }
+    }
+}
